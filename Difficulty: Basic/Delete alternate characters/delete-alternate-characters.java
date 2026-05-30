9f2e2937f563f9cf79fc55baebc8eae92a04e9b9
Ruturaj Powar <ruturajpowar2038@gmail.com>
2026-05-30 10:51:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Delete alternate characters/delete-alternate-characters.java
@@ -0,0 +1,11 @@
+class Solution {
+    static String delAlternate(String s) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i =0; i<s.length();i+=2){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
